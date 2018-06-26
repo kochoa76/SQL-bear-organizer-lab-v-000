a329a3305c08fd83172eeaae3dff84bbf8e3f8bd
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-26 17:36:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "calm", "Y"); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Yellow", "angry", "N")
