8bee9962e0e4775c75dd1e19e1c17fbdca25ee4e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-26 17:26:55 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,2 @@
 CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive TEXT);
-INSERT INTO bears (name) VALUES(8); 
+INSERT INTO bears (id ) VALUES(8); 
